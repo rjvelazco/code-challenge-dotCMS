f08226858dfe3266d931d4cee36d83360e025966
rjvelazco <rjvelazco21@gmail.com>
2021-02-23 18:46:10 -0400

    Animation Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
 
     
     // ENDPOINTS    
-    const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500/fp/.65,.37', '/focalpoints/fp-x=45%/f-py=45%/scale(1.2)', '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0',''];
+    const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500/fp/.65,.37', '/focalpointsf-py=45%/', 'p-x=45%/', '/scale(1.2)', '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0',''];
  
     class TypeWriter{
 
@@ -75,18 +75,27 @@
                 img.classList.toggle('cropped');
             break;
             case 2:
-                showFocalPoints();
+                // showFocalPoints();
+                showFocalLeft();
             break;
             case 3:
-                img.classList.add('grey-scale');
+                // showFocalPoints();
+                showFocalBottom();
             break;
             case 4:
-                img.classList.toggle('hsb');
+                // showFocalPoints();
+                hideFocalPoints();
+            break;
+            case 5:
+                img.classList.add('grey-scale');
+            break;
+            case 6:
                 if(img.classList.contains('grey-scale')){
                     img.classList.remove('grey-scale');
                 }
+                img.classList.toggle('hsb');
             break;
-            case 5:
+            case 7:
                if(reset){
                    resetAnimations();
                }
@@ -98,8 +107,9 @@
 
     
     const resetAnimations = ()=>{
+        console.log('Hola');
         toggleShowClass();
-        img.classList.remove('zoomScale');
+        imgContainer.classList.remove('zoomScale');
         setTimeout(()=>toggleShowClass(), 1200);
     }
     
@@ -109,6 +119,24 @@
         setTimeout(()=>{
             imgContainer.classList.remove('move');
             img.classList.add('zoomScale');
+        }, 1500);
+    }
+
+    const showFocalLeft = ()=>{
+        imgContainer.classList.add('showLeft');
+        setTimeout(()=>imgContainer.classList.add('moveLeft'), 1000);
+    }
+
+    const showFocalBottom = ()=>{
+        imgContainer.classList.add('showBottom');
+        setTimeout(()=>imgContainer.classList.add('moveBottom'), 1000);
+    }
+    
+    const hideFocalPoints = () =>{
+        imgContainer.classList.add('zoomScale');
+        setTimeout(()=>{
+            imgContainer.classList.remove('showBottom');
+            imgContainer.classList.remove('showLeft');
         }, 1500);
     }
     
@@ -131,3 +159,136 @@
 
     typeWriter.type();
 })();
+// (()=>{
+
+//     const img = document.querySelector('.img'),
+//     imgContainer = document.querySelector('.imgContainer'),
+//     contentText = document.querySelector('.card-body'),
+//     endPointContainer = document.querySelector('#endPoint'),
+//     blinkingCursor = document.querySelector('.cursor-effect');
+
+    
+//     // ENDPOINTS    
+//     const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500/fp/.65,.37', '/focalpoints/fp-x=45%/f-py=45%/scale(1.2)', '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0',''];
+ 
+//     class TypeWriter{
+
+//         constructor(element, words, time = 5000){
+
+//             // Set properties
+//             this.element = element;
+//             this.words = words;
+//             this.time = parseInt(time, 10);
+
+//             // New properties
+//             this.txt = '';
+//             this.wordsIndex = 0;
+//             this.isDeleting = false;
+//         }
+
+
+//         type(){
+//             const currentIndex = this.wordsIndex;
+//             const fullTxt = this.words[currentIndex];
+//             let typeSpeed = (this.isDeleting)? 200: 100;
+            
+//             (this.isDeleting)
+//                     ? this.txt = fullTxt.substring(0, this.txt.length - 1)
+//                     : this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+//             this.element.innerHTML = `${this.txt}`;
+
+
+//             if(!this.isDeleting && this.txt === fullTxt){
+
+//                 this.isDeleting = true;
+//                 typeSpeed = this.time;
+
+//                 toggleBlinkingEffect(typeSpeed);
+//                 toggleTransition(currentIndex, true);
+//                 deleteWord();
+
+//             } else if(this.isDeleting && this.txt === '') {
+//                 this.isDeleting = false;
+
+//                 (this.wordsIndex === (this.words.length -1))
+//                 ? this.wordsIndex = 0
+//                 : this.wordsIndex++;
+
+//                 typeSpeed = 1000;
+//                 toggleTransition(currentIndex);
+//                 toggleBlinkingEffect(typeSpeed);
+//             }
+
+//             setTimeout(()=>this.type(), typeSpeed);
+//         }
+//     }
+
+//     const toggleTransition = (wordIndex, reset = false) =>{
+//         switch (wordIndex){
+//             case 0:
+//                 img.classList.add('resized');
+//             break;
+//             case 1:
+//                 if(img.classList.contains('cropped')){
+//                     setTimeout(img.classList.remove('resized'), 1000);
+//                 }
+//                 img.classList.toggle('cropped');
+//             break;
+//             case 2:
+//                 showFocalPoints();
+//             break;
+//             case 3:
+//                 img.classList.add('grey-scale');
+//             break;
+//             case 4:
+//                 img.classList.toggle('hsb');
+//                 if(img.classList.contains('grey-scale')){
+//                     img.classList.remove('grey-scale');
+//                 }
+//             break;
+//             case 5:
+//                if(reset){
+//                    resetAnimations();
+//                }
+//             break;
+//         }
+//     }
+
+//     const toggleShowClass = () => contentText.classList.toggle('show');
+
+    
+//     const resetAnimations = ()=>{
+//         toggleShowClass();
+//         img.classList.remove('zoomScale');
+//         setTimeout(()=>toggleShowClass(), 1200);
+//     }
+    
+//     const showFocalPoints = ()=>{
+//         imgContainer.classList.toggle('show');
+//         setTimeout(()=>imgContainer.classList.add('move'), 1000);
+//         setTimeout(()=>{
+//             imgContainer.classList.remove('move');
+//             img.classList.add('zoomScale');
+//         }, 1500);
+//     }
+    
+//     const toggleBlinkingEffect = (Speed)=>{
+//         blinkingCursor.classList.add('blinking');
+//         setTimeout(()=>blinkingCursor.classList.remove('blinking'), Speed);
+//     }
+
+//     const deleteWord = ()=>{
+//         setTimeout(()=>endPointContainer.classList.add('highlight'), 1500);
+//         setTimeout(()=>{;
+//             typeWriter.txt = '';
+//             typeWriter.element.innerHTML = '';
+//             endPointContainer.classList.remove('highlight');
+//         }, 2500);
+//     }
+
+//     const typeWriter = new TypeWriter(endPointContainer, endPoints, 2500);
+//     toggleShowClass();
+
+//     typeWriter.type();
+// })();
