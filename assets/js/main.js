3b4db20d066687a9995c2e177fa89df081674ca9
rjvelazco <rjvelazco21@gmail.com>
2021-02-24 11:20:21 -0400

    Completed - Waiting for tests

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,22 @@
     blinkingCursor = document.querySelector('.cursor-effect');
 
     
+
     // ENDPOINTS    
-    const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500/fp/.65,.37', '/focalpointsf-py=45%/', 'p-x=45%/', '/scale(1.2)', '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0',''];
+    const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500', true, '/fp/.69', ',.67', '/crop_w/500/crop_h/500/', true, '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0', ''];
  
+
+    // Times
+    const time500s = 500,
+    time1000s = 1000,
+    time1500s = 1500,
+    time2000s = 2000;
+    time2500s = 2500;
+    time3000s = 3000;
+
     class TypeWriter{
 
-        constructor(element, words, time = 5000){
+        constructor(element, words, time){
 
             // Set properties
             this.element = element;
@@ -23,82 +33,96 @@
             this.txt = '';
             this.wordsIndex = 0;
             this.isDeleting = false;
+
+            this.allTxt = '';
         }
 
 
         type(){
             const currentIndex = this.wordsIndex;
-            const fullTxt = this.words[currentIndex];
+            let fullTxt = this.words[currentIndex];
             let typeSpeed = (this.isDeleting)? 200: 100;
             
-            (this.isDeleting)
-                    ? this.txt = fullTxt.substring(0, this.txt.length - 1)
-                    : this.txt = fullTxt.substring(0, this.txt.length + 1);
+            if(currentIndex === this.words.length - 1){
+
+                this.allTxt = '';
+                this.wordsIndex = 0;
+                resetLoop();
+                setTimeout(()=>this.type(), time3000s);
+                return;
+
+            }
+
+            if(this.nextWord && this.allTxt.length > 0){
+                this.txt = this.allTxt;
+                this.nextWord = false;
+            }
+
+            if(typeof fullTxt === 'boolean'){
+                this.allTxt = '';
+                this.wordsIndex++;
+                deleteWord();
+                setTimeout(()=>toggleTransition(currentIndex-1), time1000s);
+                setTimeout(()=>this.type(), time2500s);
+                return;
+            }
+
+            fullTxt = this.allTxt + this.words[currentIndex];
+
+            
+            if(this.allTxt.length > 0){
+                // add the word before to the new word
+                this.txt = this.allTxt + fullTxt.substring(this.allTxt.length, this.txt.length + 1);
+            } else{
+                // set a new word
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
 
             this.element.innerHTML = `${this.txt}`;
 
-
-            if(!this.isDeleting && this.txt === fullTxt){
-
-                this.isDeleting = true;
+            if(this.txt === fullTxt){
+                this.allTxt = this.txt;
+                this.txt = '';
                 typeSpeed = this.time;
 
-                toggleBlinkingEffect(typeSpeed);
+        
+                this.wordsIndex++;
+                this.nextWord = true;
+
+                toggleBlinkingEffect(time1000s);
                 toggleTransition(currentIndex, true);
-                deleteWord();
-
-            } else if(this.isDeleting && this.txt === '') {
-                this.isDeleting = false;
-
-                (this.wordsIndex === (this.words.length -1))
-                ? this.wordsIndex = 0
-                : this.wordsIndex++;
-
-                typeSpeed = 1000;
-                toggleTransition(currentIndex);
-                toggleBlinkingEffect(typeSpeed);
-            }
+            } 
 
             setTimeout(()=>this.type(), typeSpeed);
         }
     }
-
-    const toggleTransition = (wordIndex, reset = false) =>{
+    
+    
+    const toggleTransition = (wordIndex) =>{
         switch (wordIndex){
             case 0:
                 img.classList.add('resized');
             break;
             case 1:
                 if(img.classList.contains('cropped')){
-                    setTimeout(img.classList.remove('resized'), 1000);
+                    setTimeout(img.classList.remove('resized'), time1000s);
                 }
                 img.classList.toggle('cropped');
             break;
-            case 2:
-                // showFocalPoints();
+            case 3:
                 showFocalLeft();
             break;
-            case 3:
-                // showFocalPoints();
+            case 4:
                 showFocalBottom();
             break;
-            case 4:
-                // showFocalPoints();
+            case 5:
                 hideFocalPoints();
             break;
-            case 5:
+            case 7:
                 img.classList.add('grey-scale');
             break;
-            case 6:
-                if(img.classList.contains('grey-scale')){
-                    img.classList.remove('grey-scale');
-                }
-                img.classList.toggle('hsb');
-            break;
-            case 7:
-               if(reset){
-                   resetAnimations();
-               }
+            case 8:
+                img.classList.add('hsb');
             break;
         }
     }
@@ -107,29 +131,19 @@
 
     
     const resetAnimations = ()=>{
-        console.log('Hola');
         toggleShowClass();
         imgContainer.classList.remove('zoomScale');
-        setTimeout(()=>toggleShowClass(), 1200);
+        setTimeout(()=>toggleShowClass(), time1000s);
     }
     
-    const showFocalPoints = ()=>{
-        imgContainer.classList.toggle('show');
-        setTimeout(()=>imgContainer.classList.add('move'), 1000);
-        setTimeout(()=>{
-            imgContainer.classList.remove('move');
-            img.classList.add('zoomScale');
-        }, 1500);
-    }
-
     const showFocalLeft = ()=>{
         imgContainer.classList.add('showLeft');
-        setTimeout(()=>imgContainer.classList.add('moveLeft'), 1000);
+        setTimeout(()=>imgContainer.classList.add('moveLeft'), time1000s);
     }
 
     const showFocalBottom = ()=>{
         imgContainer.classList.add('showBottom');
-        setTimeout(()=>imgContainer.classList.add('moveBottom'), 1000);
+        setTimeout(()=>imgContainer.classList.add('moveBottom'), time1000s);
     }
     
     const hideFocalPoints = () =>{
@@ -137,7 +151,7 @@
         setTimeout(()=>{
             imgContainer.classList.remove('showBottom');
             imgContainer.classList.remove('showLeft');
-        }, 1500);
+        }, 1800);
     }
     
     const toggleBlinkingEffect = (Speed)=>{
@@ -146,149 +160,33 @@
     }
 
     const deleteWord = ()=>{
-        setTimeout(()=>endPointContainer.classList.add('highlight'), 1500);
+        setTimeout(()=>endPointContainer.classList.add('highlight'), time500s);
         setTimeout(()=>{;
+            // typeWriter.isDeleting = true;
             typeWriter.txt = '';
             typeWriter.element.innerHTML = '';
             endPointContainer.classList.remove('highlight');
-        }, 2500);
+        }, 1000);
     }
 
-    const typeWriter = new TypeWriter(endPointContainer, endPoints, 2500);
+    const resetLoop = () =>{
+        deleteWord();
+
+        setTimeout(()=>{
+            imgContainer.classList.remove('moveBottom');
+            imgContainer.classList.remove('moveLeft');
+            img.classList.remove('grey-scale');
+            img.classList.remove('hsb');
+            resetAnimations();  
+        }, 1000)
+    }
+
+    const typeWriter = new TypeWriter(endPointContainer, endPoints, time1000s);
+
+    setTimeout(()=>{
+
+        typeWriter.type();
+    }, time1500s);
     toggleShowClass();
 
-    typeWriter.type();
 })();
-// (()=>{
-
-//     const img = document.querySelector('.img'),
-//     imgContainer = document.querySelector('.imgContainer'),
-//     contentText = document.querySelector('.card-body'),
-//     endPointContainer = document.querySelector('#endPoint'),
-//     blinkingCursor = document.querySelector('.cursor-effect');
-
-    
-//     // ENDPOINTS    
-//     const endPoints = ['/resize_w/800/resize_h/600', '/crop_w/500/crop_h/500/fp/.65,.37', '/focalpoints/fp-x=45%/f-py=45%/scale(1.2)', '/filter/Grayscale', '/hsb_h/-1.0/hsb_s/0.0/hsb_b/1.0',''];
- 
-//     class TypeWriter{
-
-//         constructor(element, words, time = 5000){
-
-//             // Set properties
-//             this.element = element;
-//             this.words = words;
-//             this.time = parseInt(time, 10);
-
-//             // New properties
-//             this.txt = '';
-//             this.wordsIndex = 0;
-//             this.isDeleting = false;
-//         }
-
-
-//         type(){
-//             const currentIndex = this.wordsIndex;
-//             const fullTxt = this.words[currentIndex];
-//             let typeSpeed = (this.isDeleting)? 200: 100;
-            
-//             (this.isDeleting)
-//                     ? this.txt = fullTxt.substring(0, this.txt.length - 1)
-//                     : this.txt = fullTxt.substring(0, this.txt.length + 1);
-
-//             this.element.innerHTML = `${this.txt}`;
-
-
-//             if(!this.isDeleting && this.txt === fullTxt){
-
-//                 this.isDeleting = true;
-//                 typeSpeed = this.time;
-
-//                 toggleBlinkingEffect(typeSpeed);
-//                 toggleTransition(currentIndex, true);
-//                 deleteWord();
-
-//             } else if(this.isDeleting && this.txt === '') {
-//                 this.isDeleting = false;
-
-//                 (this.wordsIndex === (this.words.length -1))
-//                 ? this.wordsIndex = 0
-//                 : this.wordsIndex++;
-
-//                 typeSpeed = 1000;
-//                 toggleTransition(currentIndex);
-//                 toggleBlinkingEffect(typeSpeed);
-//             }
-
-//             setTimeout(()=>this.type(), typeSpeed);
-//         }
-//     }
-
-//     const toggleTransition = (wordIndex, reset = false) =>{
-//         switch (wordIndex){
-//             case 0:
-//                 img.classList.add('resized');
-//             break;
-//             case 1:
-//                 if(img.classList.contains('cropped')){
-//                     setTimeout(img.classList.remove('resized'), 1000);
-//                 }
-//                 img.classList.toggle('cropped');
-//             break;
-//             case 2:
-//                 showFocalPoints();
-//             break;
-//             case 3:
-//                 img.classList.add('grey-scale');
-//             break;
-//             case 4:
-//                 img.classList.toggle('hsb');
-//                 if(img.classList.contains('grey-scale')){
-//                     img.classList.remove('grey-scale');
-//                 }
-//             break;
-//             case 5:
-//                if(reset){
-//                    resetAnimations();
-//                }
-//             break;
-//         }
-//     }
-
-//     const toggleShowClass = () => contentText.classList.toggle('show');
-
-    
-//     const resetAnimations = ()=>{
-//         toggleShowClass();
-//         img.classList.remove('zoomScale');
-//         setTimeout(()=>toggleShowClass(), 1200);
-//     }
-    
-//     const showFocalPoints = ()=>{
-//         imgContainer.classList.toggle('show');
-//         setTimeout(()=>imgContainer.classList.add('move'), 1000);
-//         setTimeout(()=>{
-//             imgContainer.classList.remove('move');
-//             img.classList.add('zoomScale');
-//         }, 1500);
-//     }
-    
-//     const toggleBlinkingEffect = (Speed)=>{
-//         blinkingCursor.classList.add('blinking');
-//         setTimeout(()=>blinkingCursor.classList.remove('blinking'), Speed);
-//     }
-
-//     const deleteWord = ()=>{
-//         setTimeout(()=>endPointContainer.classList.add('highlight'), 1500);
-//         setTimeout(()=>{;
-//             typeWriter.txt = '';
-//             typeWriter.element.innerHTML = '';
-//             endPointContainer.classList.remove('highlight');
-//         }, 2500);
-//     }
-
-//     const typeWriter = new TypeWriter(endPointContainer, endPoints, 2500);
-//     toggleShowClass();
-
-//     typeWriter.type();
-// })();
